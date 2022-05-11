df0ae973298cd52153d3c08bc643eda376b2f036
Christelle Georges <78320504+Christellejg@users.noreply.github.com>
2022-05-10 23:20:23 -0400

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -12,3 +12,9 @@ Suspension_coil <- read.csv('Suspension_Coil.csv',check.names = F, stringsAsFact
 # Create summary table for suspension coils
 total_summary <- Suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Std_Dev=sd(PSI))
 lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Std_Dev=sd(PSI))
+### Deliverable 3 ####
+# t-test
+t.test(Suspension_coil$PSI, mu=1500)
+t.test(subset(Suspension_coil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(Suspension_coil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(Suspension_coil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
